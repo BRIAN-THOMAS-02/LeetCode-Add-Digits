d4eb4981ebebf359da7967b9993eeda364184948
BRIAN THOMAS <87309254+BRIAN-THOMAS-02@users.noreply.github.com>
2022-07-18 00:17:51 +0530

    Create Add Digits - Submission.cpp

---
+++ Add Digits - Submission.cpp
@@ -0,0 +1,23 @@
+Add Digits - "https://leetcode.com/problems/add-digits/"
+SUBMISSION
+
+class Solution 
+{
+public:
+    int addDigits(int num) 
+    {
+        int sum = 0;
+        while(num > 0)
+        {
+            int last = num % 10;
+            sum = sum + last;
+            num = num / 10;
+            if(num == 0 && sum > 9)
+            {
+                num = sum;
+                sum = 0;
+            }
+        }
+    return sum;
+    }
+};
